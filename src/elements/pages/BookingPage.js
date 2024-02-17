6d9a83cf14f8a12077ca4e7e8224928f5c7a968e
gustavo-c-dias <chimenes61@gmail.com>
2024-02-17 15:41:31 -0300

    bookingPage

---
+++ src/elements/pages/BookingPage.js
@@ -0,0 +1,20 @@
+
+import Header from '../global/Header';
+import Nav from '../organismo/Nav';
+import Footer from '../global/Footer';
+
+function BookingPage() {
+  return (
+    <>
+        <Header urlImage='./assets/logo.svg'>
+        <Nav/>
+        </Header>
+
+    <main>
+    </main>
+
+    <Footer/>
+    </>
+  )
+}
+export default BookingPage;
